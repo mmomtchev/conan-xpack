c95f5a562bb8cd8d424264da6f952e363805f7b7
Momtchil Momtchev <momtchil@momtchev.com>
2024-05-30 18:08:36 +0200

    ensure conan is initialized

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # 2.3.2-2
   Automatically set `CONAN_HOME` to a well-known `hadron` default
+  Automatically create the `conan` profile
 
 # 2.3.2-1 2024-05-30
   First release
--- wrappers/conan.cmd
+++ wrappers/conan.cmd
@@ -1,9 +1,8 @@
 @ECHO OFF
 SET MYPATH=%~dp0
 SET PYTHONPATH=%MYPATH:~0,-1%\..
-IF NOT DEFINED CONAN_HOME (
-  SET CONAN_HOME=%LOCALAPPDATA%\hadron
-)
+IF NOT DEFINED CONAN_HOME (SET CONAN_HOME=%LOCALAPPDATA%\hadron)
 ECHO "conan building packages in %CONAN_HOME%"
 ECHO "  This directory can grow to a considerable size"
+IF NOT EXIST %CONAN_HOME%\profiles\default (%MYPATH:~0,-1%\_conan.exe profile detect)
 %MYPATH:~0,-1%\_conan.exe %*
