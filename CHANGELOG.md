3d7e9717107d416e46c130cdd912401de2319835
Momtchil Momtchev <momtchil@momtchev.com>
2024-05-30 15:20:09 +0200

    set CONAN_HOME to a hadron default

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 2.3.2-2
+  Automatically set `CONAN_HOME` to a well-known `hadron` default
+
+# 2.3.2-1 2024-05-30
+  First release
--- wrappers/conan.cmd
+++ wrappers/conan.cmd
@@ -1,4 +1,9 @@
 @ECHO OFF
 SET MYPATH=%~dp0
 SET PYTHONPATH=%MYPATH:~0,-1%\..
+IF NOT DEFINED CONAN_HOME (
+  SET CONAN_HOME=%LOCALAPPDATA%\hadron
+)
+ECHO "conan building packages in %CONAN_HOME%"
+ECHO "  This directory can grow to a considerable size"
 %MYPATH:~0,-1%\_conan.exe %*
